6c0750cb4b7e659b5c2cdbb43c5c3557e0e71c00
notnaveen222 <notnaveen2@gmail.com>
2024-11-11 12:42:43 +0530

    Navbar mobile spacing Fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="z-10 fixed w-full px-[30px] pt-[10px]">
+      <div className="z-10 fixed w-full px-[18px] sm:px-[30px] pt-[10px]">
         <div className="backdrop-blur-xl  text-white  w-full rounded-full top-2 h-[60px] px-[30px] flex justify-between items-center">
           <img className="h-10" src={naveenLogo} alt="" />
           <div className="flex text-[15px] font-medium font-interT gap-x-6 ">
