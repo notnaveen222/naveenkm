c263fdf76a85acf05b5ba869a9ad7b5abbbfe13d
notnaveen222 <notnaveen2@gmail.com>
2024-11-13 15:51:42 +0530

    horizontal scroll check commit sequence

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,9 +5,7 @@ const About = () => {
   return (
     <div className="flex flex-col items-center  max-w-[1240px] mx-auto text-white  ">
       <div className="mb-10 text-4xl text-white sm:text-7xl">About</div>
-      <div className="px-8 mb-20 sm:mb-32 sm:px-0">
-        <BentoGrid />
-      </div>
+      <div className="px-8 mb-20 sm:mb-32 sm:px-0"></div>
     </div>
   );
 };
